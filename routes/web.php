a9ca58944484e66b2e0281e1114bdfbcb61d35b3
DarahBeku <bryan.535230012@stu.untar.ac.id>
2024-06-19 11:37:48 +0700

    UI

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,8 @@
 <?php
 use App\Http\Controllers\AuthController;
 use Illuminate\Support\Facades\Route;
+use Illuminate\Support\Facades\File;
+use Illuminate\Support\Facades\Response;
 
 Route::get('/', function () {
     return view('welcome');
@@ -10,8 +12,24 @@ Route::middleware("auth")->group(function(){
     Route::view("/", "welcome")->name("home");
 });
 
+Route::get('/css/app.css', function () {
+    $path = resource_path('css/app.css');
+
+    if (!File::exists($path)) {
+        abort(404);
+    }
+
+    $file = File::get($path);
+    $type = File::mimeType($path);
+
+    $response = Response::make($file, 200);
+    $response->header("Content-Type", $type);
+
+    return $response;
+});
+
 Route::get("/login", [AuthController::class, "login"])->name("login");
 Route::post("/login", [AuthController::class, "loginPost"])->name("login.post");
+Route::get('/register', [RegisterController::class, 'showRegistrationForm'])->name('register');
 Route::get("/register", [AuthController::class, "register"])->name("register");
 Route::post("/register", [AuthController::class, "registerPost"])->name("register.post");
-Route::get('/register', [RegisterController::class, 'showRegistrationForm'])->name('register');
